313c16c3c663b4c07e4eea9a7a538a52c76748a3
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2024-07-16 00:27:12 +0900

    0715 메인타이틀 수정 이어서하기

--- src/pages/Logopage.js
+++ src/pages/Logopage.js
@@ -40,79 +40,59 @@ const Logopage = () => {
     <div className="main-page">
       <section className="hero">
         <div className="hero-content">
-          <h1 className="hero-title">Welcome to Our Website</h1>
+          <h1 className="hero-title">GREAP에 오신것을 환영합니다!</h1>
           <p className="hero-text">
-            Discover the best services and products for your needs.
+            친한 사람들과 본인의 이야기를 공유해보세요
           </p>
-          <a href="#services" className="btn-primary">
-            <Link to="/home" className="button" onClick={handleViewClick}>
-              바로보기
-            </Link>{" "}
-          </a>
-          {!user && ( // user가 없으면 (로그인이 되어 있지 않으면)
-            <p style={{ textAlign: "center", marginTop: "20px", color: "red" }}>
-              로그인이 필요합니다.
-            </p>
-          )}
         </div>
       </section>
 
       <section className="features" id="features">
         <div className="container">
-          <h2 className="section-title">Key Features</h2>
+          <h2 className="section-title">기능소개</h2>
           <div className="feature-box">
             <div className="feature-item">
-              <h3 className="feature-item-title">Feature 1</h3>
+              <h3 className="feature-item-title">사람과의 연결</h3>
               <p className="feature-item-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                친구 또는 지인 혹은 모르는 사람과 Follow라는 인연을 맺어보세요.
               </p>
             </div>
             <div className="feature-item">
-              <h3 className="feature-item-title">Feature 2</h3>
+              <h3 className="feature-item-title">순간의 저장</h3>
               <p className="feature-item-text">
-                Sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua.
+                당신이 무엇을 하는지를 공유하여 보세요!
               </p>
             </div>
             <div className="feature-item">
-              <h3 className="feature-item-title">Feature 3</h3>
+              <h3 className="feature-item-title">새로운 탐색</h3>
               <p className="feature-item-text">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat.
+                당신의 소중한 순간을 보낼 동안, 다른사람은 어떤 소중한 순간을
+                보냈는지 구경해보세요!
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="services" id="services">
+      {/* <section className="services" id="services">
         <div className="container">
-          <h2 className="section-title">Our Services</h2>
+          <h2 className="section-title">서브내용 2</h2>
           <div className="service-box">
             <div className="service-item">
-              <h3 className="service-item-title">Service 1</h3>
-              <p className="service-item-text">
-                Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua.
-              </p>
+              <h3 className="service-item-title">미니타이틀 1</h3>
+              <p className="service-item-text">내용</p>
             </div>
             <div className="service-item">
-              <h3 className="service-item-title">Service 2</h3>
-              <p className="service-item-text">
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat.
-              </p>
+              <h3 className="service-item-title">미니타이틀 2</h3>
+              <p className="service-item-text">내용</p>
             </div>
             <div className="service-item">
-              <h3 className="service-item-title">Service 3</h3>
-              <p className="service-item-text">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.
-              </p>
+              <h3 className="service-item-title">미니타이틀 3</h3>
+              <p className="service-item-text">내용</p>
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
