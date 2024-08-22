aa6b438e4b06481cb6dc3b96b35b1a7e3e710422
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2024-08-23 06:21:48 +0900

    0823 부족한거 추가..

--- src/components/ImageSlideshow.js
+++ src/components/ImageSlideshow.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import "./ImageSlideshow.css"; // 스타일 시트 import
 
 const images = [
-  "/사진/포트폴리오1.JPG",
-  "/사진/포트폴리오2.JPG",
-  "/사진/포트폴리오2.5.JPG",
-  "/사진/포트폴리오3.JPG",
-  "/사진/포트폴리오4.JPG",
-  "/사진/포트폴리오5.JPG",
-  "/사진/포트폴리오6.JPG",
-  "/사진/포트폴리오7.JPG",
+  "/사진/로고페이지1.JPG",
+  "/사진/로고페이지2.JPG",
+  "/사진/로고페이지3.JPG",
 
-  "/사진/포트폴리오-1.JPG",
-  "/사진/포트폴리오-2.JPG",
-  "/사진/포트폴리오-3.JPG",
-  "/사진/포트폴리오-4.JPG",
+  "/사진/포트폴리오1홈.JPG",
+  "/사진/포트폴리오2게시물.JPG",
+  "/사진/포트폴리오3사이드메뉴.JPG",
+  "/사진/포트폴리오4업로드페이지.JPG",
+  "/사진/포트폴리오5다이렉트메세지.JPG",
+  "/사진/포트폴리오5알림.JPG",
+  "/사진/포트폴리오6프로필.JPG",
+
+  "/사진/포트폴리오7반응형팔로워리스트.JPG",
+  "/사진/포트폴리오7반응형홈.JPG",
+  "/사진/포트폴리오8반응형메세지.JPG",
+  "/사진/포트폴리오9반응형프로필.JPG",
+  "/사진/포트폴리오10반응형업로드.JPG",
   // 여기에 모든 이미지 경로 추가
 ];
 
@@ -40,6 +44,9 @@ const ImageSlideshow = () => {
         alt={`Slide ${currentImageIndex}`}
         className="slide-image"
       />
+      <div className="slide-counter">
+        {currentImageIndex + 1} / {images.length}
+      </div>
       <button className="prev-button" onClick={handlePrevClick}>
         &#10094;
       </button>
